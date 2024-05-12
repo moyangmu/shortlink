5044df237f9ab2ede10cd975418e92bf11e5900a
moyangmu <729227315@qq.com>
2024-05-12 16:54:19 +0800

    优化用户持久层实体基础属性对象抽象

--- admin/src/main/java/org/example/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/org/example/shortlink/admin/dao/entity/UserDO.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
+import org.example.shortlink.admin.common.database.BaseDO;
 
 import java.util.Date;
 
@@ -12,7 +13,7 @@ import java.util.Date;
  */
 @Data
 @TableName("t_user")
-public class UserDO {
+public class UserDO extends BaseDO {
 
     /**
      * id
@@ -49,22 +50,4 @@ public class UserDO {
      */
     private Long deletionTime;
 
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
-
-    /**
-     * 修改时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    /**
-     * 删除标识 0：未删除 1：已删除
-     */
-    @TableField(fill = FieldFill.INSERT)
-
-    private int delFlag;
 }
